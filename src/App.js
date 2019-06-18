10afef8e15421dad142cd69088059cfec75d6763
aleisabl <aleisabl.25@gmail.com>
2019-06-18 19:55:07 -0400

    primer intento de renderizar mapa. for each de axios.get necesita arreglo y separar componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
-import GetShops from './GetShops.js'
+
+/* dataUrl = 'https://raw.githubusercontent.com/aleisabl/generation-take-home-intern/master/src/store_directory.json' */
 
 class App extends Component {
 
@@ -13,8 +14,45 @@ class App extends Component {
   renderMap = () => {
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCOzgQyguR0MhgN_xU6-WdLzGgjId5wZJY&callback=initMap')
 
-    window.map = this.map
     window.initMap = this.initMap
+  }
+
+  getShops = () => {
+    const endPoint = 'https://raw.githubusercontent.com/aleisabl/generation-take-home-intern/master/src/store_directory.json'
+
+    axios.get(endPoint /* + new URLSearchParams(parameters) */)
+    .then(response => {
+      this.data = response.data;
+
+      this.data.forEach((item) => {
+        console.log("found: ", item)
+        console.log("found id: ", item.Coordinates)
+
+        let map = new window.google.maps.Map(document.getElementById('map'), {
+          center: {lat: 19.432608, lng: -99.133209},
+          zoom: 13.5
+        });
+
+        let markerLatlng = new window.google.maps.LatLng(item.Coordinates.lat, item.Coordinates.lng);
+        let marker = new window.google.maps.Marker({
+          position: markerLatlng, map:map
+      });
+      marker.item = item; // store information of each item in marker
+        
+      marker.setMap(map); // where `map` is the instance of Google Map
+      window.google.maps.event.addListener(marker, "click", function() {
+        alert('hola')
+          // retrieve information using `this.item` and create dynamic HTML to show it. 
+          // this.item.Post.datetime, this.item.Post.content etc.
+      });
+
+      });
+
+     
+    })
+    .catch(error => {
+      console.log('auxilio!! ' + error)
+    })
   }
 
   initMap = () => {
@@ -24,17 +62,11 @@ class App extends Component {
     });
   }
 
-
-  
-/*    let markers = new window.google.maps.Marker({position: {lat: 19.432608, lng: -99.133209}, map: map});
-    let markerCluster = new MarkerClusterer(map, markers,
-      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'}); */
   render() {
 
     return (
       <main>
         <div id="map">
-        <GetShops/>
         </div>
       </main>
      
@@ -53,3 +85,41 @@ class App extends Component {
 
 
 export default App;
+
+
+ /* const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: '0CJOAQBDVLTUTOZEYIT2MGWKC0FRYVYIRJTYZGL4RLGFUZSZ',
+      client_secret: 'HTJ1LKWHJJFN1EZIB1YSPBC0LU2UPXQOTORE4Y25KQVTU40Q',
+      query: 'Red Barn Stores',
+      near: 'Mexico',
+      v: '20180323'
+    } */
+
+    /*    let markers = new window.google.maps.Marker({position: {lat: 19.432608, lng: -99.133209}, map: map});
+    let markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'}); */
+
+
+       /* for (var i = 0; i < response.data.length; i++) {
+        var item = response.data[i];
+        let map = new window.google.maps.Map(document.getElementById('map'), {
+          center: {lat: 19.432608, lng: -99.133209},
+          zoom: 13.5
+        });
+        console.log(item)
+    
+        var markerLatlng = new window.google.maps.LatLng(item.Coordinates.lat, item.Coordinates.lng);
+        var marker = new window.google.maps.Marker({
+            position: markerLatlng, map:map
+        });
+        marker.item = item; // store information of each item in marker
+        
+        marker.setMap(map); // where `map` is the instance of Google Map
+        window.google.maps.event.addListener(marker, "click", function() {
+          alert('hola')
+            // retrieve information using `this.item` and create dynamic HTML to show it. 
+            // this.item.Post.datetime, this.item.Post.content etc.
+        });
+    
+    } */
