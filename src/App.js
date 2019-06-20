615f3bbc8f68ac6156ad98013a3fa181091eadc3
aleisabl <aleisabl.25@gmail.com>
2019-06-20 17:20:29 -0400

    Se carga el script con una funcion 'loadScript' en vez de con libreria google-maps-react. Problema con loop.

--- src/App.js
+++ src/App.js
@@ -1,125 +1,94 @@
-import React, { Component } from 'react';
-import './App.css';
+
+import React, { Component } from 'react'
+import './App.css'
 import axios from 'axios'
 
-/* dataUrl = 'https://raw.githubusercontent.com/aleisabl/generation-take-home-intern/master/src/store_directory.json' */
+class YourComponent extends Component {
 
-class App extends Component {
+  state = {
+    Coordinates: []
+  }
 
+  //First it runs getCoordinates wich it fetchs the url of the sores and passes it
   componentDidMount() {
-    this.getShops()
-    this.renderMap()
+    this.getCoordinates()
   }
 
   renderMap = () => {
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCOzgQyguR0MhgN_xU6-WdLzGgjId5wZJY&callback=initMap')
-
     window.initMap = this.initMap
   }
 
-  getShops = () => {
-    const endPoint = 'https://raw.githubusercontent.com/aleisabl/generation-take-home-intern/master/src/store_directory.json'
+  getCoordinates = () => {
+    const endPoint = "https://raw.githubusercontent.com/aleisabl/generation-take-home-intern/master/src/store_directory.json"
 
-    axios.get(endPoint /* + new URLSearchParams(parameters) */)
-    .then(response => {
-      this.data = response.data;
+    axios.get(endPoint)
+      .then(response => {
 
-      this.data.forEach((item) => {
-        console.log("found: ", item)
-        console.log("found id: ", item.Coordinates)
+        for (var i = 0; i < response.data.length; i++) {
 
-        let map = new window.google.maps.Map(document.getElementById('map'), {
-          center: {lat: 19.432608, lng: -99.133209},
-          zoom: 13.5
-        });
+          this.setState({
+            Coordinates: response.data[i]
+          }, this.renderMap()) //redering renderMap (loadscript) as a Callback after our state is refreshed, otherwise it crashes
 
-        let markerLatlng = new window.google.maps.LatLng(item.Coordinates.lat, item.Coordinates.lng);
-        let marker = new window.google.maps.Marker({
-          position: markerLatlng, map:map
-      });
-      marker.item = item; // store information of each item in marker
+        }
         
-      marker.setMap(map); // where `map` is the instance of Google Map
-      window.google.maps.event.addListener(marker, "click", function() {
-        alert('hola')
-          // retrieve information using `this.item` and create dynamic HTML to show it. 
-          // this.item.Post.datetime, this.item.Post.content etc.
-      });
+      })
+      .catch(error => {
+        console.log("ERROR!! " + error)
+      })
 
-      });
-
-     
-    })
-    .catch(error => {
-      console.log('auxilio!! ' + error)
-    })
   }
 
   initMap = () => {
+
+    // Creating the Map
     let map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 19.432608, lng: -99.133209},
-      zoom: 13.5
-    });
+      zoom: 8
+    })
+
+    // Creating an InfoWindow
+    let infowindow = new window.google.maps.InfoWindow()
+
+    // Display the dynamic markers
+      let contentString = `Red Barn Store`
+
+      // Creating the Marker
+        let marker = new window.google.maps.Marker({
+          position: {lat: this.state.Coordinates.Coordinates.lat , lng: this.state.Coordinates.Coordinates.lng},
+          map: map
+        })
+
+      // Click on A Marker! It shows infowindow
+      marker.addListener('click', function() {
+
+        // Changing the content of indowindow passing contentstring
+        infowindow.setContent(contentString)
+
+        // Opening InfoWindow
+        infowindow.open(map, marker)
+      })
+
+
   }
 
   render() {
-
     return (
       <main>
-        <div id="map">
-        </div>
+        <div id="map"></div>
       </main>
-     
-    );
+    )
   }
 }
 
- function loadScript(url) {
-  let index = window.document.getElementsByTagName('script')[0]
-  let script = window.document.createElement('script')
-  script.src= url
+function loadScript(url) {
+  let index  = window.document.getElementsByTagName("script")[0]
+  let script = window.document.createElement("script")
+  script.src = url
   script.async = true
   script.defer = true
   index.parentNode.insertBefore(script, index)
- }
+}
 
-
-export default App;
-
-
- /* const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
-    const parameters = {
-      client_id: '0CJOAQBDVLTUTOZEYIT2MGWKC0FRYVYIRJTYZGL4RLGFUZSZ',
-      client_secret: 'HTJ1LKWHJJFN1EZIB1YSPBC0LU2UPXQOTORE4Y25KQVTU40Q',
-      query: 'Red Barn Stores',
-      near: 'Mexico',
-      v: '20180323'
-    } */
-
-    /*    let markers = new window.google.maps.Marker({position: {lat: 19.432608, lng: -99.133209}, map: map});
-    let markerCluster = new MarkerClusterer(map, markers,
-      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'}); */
-
-
-       /* for (var i = 0; i < response.data.length; i++) {
-        var item = response.data[i];
-        let map = new window.google.maps.Map(document.getElementById('map'), {
-          center: {lat: 19.432608, lng: -99.133209},
-          zoom: 13.5
-        });
-        console.log(item)
-    
-        var markerLatlng = new window.google.maps.LatLng(item.Coordinates.lat, item.Coordinates.lng);
-        var marker = new window.google.maps.Marker({
-            position: markerLatlng, map:map
-        });
-        marker.item = item; // store information of each item in marker
-        
-        marker.setMap(map); // where `map` is the instance of Google Map
-        window.google.maps.event.addListener(marker, "click", function() {
-          alert('hola')
-            // retrieve information using `this.item` and create dynamic HTML to show it. 
-            // this.item.Post.datetime, this.item.Post.content etc.
-        });
-    
-    } */
+export default YourComponent;
