f18b8c4947471b99e7942cbecb8f38b0b0d5bf9a
aleisabl <aleisabl.25@gmail.com>
2019-06-23 18:53:38 -0400

    quitando llamada de axios, en cambio:  mapeando el JSON para obtener las coordenadas de cada tienda y pasandolas a los markes dinamicos

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@
 import React, { Component } from 'react'
 import './App.css'
 import axios from 'axios'
+import { ReactComponent as Logo } from './logo.svg';
+
+const storeDirectory = require('./store_directory.json');
+
 
 class YourComponent extends Component {
 
   state = {
-    Coordinates: []
+    CoordinatesState: []
+
   }
 
   //First it runs getCoordinates wich it fetchs the url of the sores and passes it
   componentDidMount() {
-    this.getCoordinates()
+    this.renderMap()
   }
 
   renderMap = () => {
@@ -19,56 +24,46 @@ class YourComponent extends Component {
     window.initMap = this.initMap
   }
 
-  getCoordinates = () => {
-    const endPoint = "https://raw.githubusercontent.com/aleisabl/generation-take-home-intern/master/src/store_directory.json"
-
-    axios.get(endPoint)
-      .then(response => {
-
-        for (var i = 0; i < response.data.length; i++) {
-
-          this.setState({
-            Coordinates: response.data[i]
-          }, this.renderMap()) //redering renderMap (loadscript) as a Callback after our state is refreshed, otherwise it crashes
-
-        }
-        
-      })
-      .catch(error => {
-        console.log("ERROR!! " + error)
-      })
-
-  }
+ 
 
   initMap = () => {
 
     // Creating the Map
     let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 19.432608, lng: -99.133209},
+      center: { lat: 19.432608, lng: -99.133209 },
       zoom: 8
     })
 
-    // Creating an InfoWindow
-    let infowindow = new window.google.maps.InfoWindow()
+          // Creating an InfoWindow
+          let infowindow = new window.google.maps.InfoWindow()
 
-    // Display the dynamic markers
-      let contentString = `Red Barn Store`
+          // Display the dynamic markers
+          let contentString = `Red Barn Store`
 
-      // Creating the Marker
-        let marker = new window.google.maps.Marker({
-          position: {lat: this.state.Coordinates.Coordinates.lat , lng: this.state.Coordinates.Coordinates.lng},
-          map: map
-        })
+    //maping thru the JSON rather than getting the data via axios
+    //creating the marker for every position and passing name, title, etc
+    storeDirectory.map((store) => {
+      const marker = new window.google.maps.Marker({
+        position: { lat: store.Coordinates.lat, lng: store.Coordinates.lng },
+        name: store.Name,
+        title: store.Address,
+        map,
+      });
 
-      // Click on A Marker! It shows infowindow
-      marker.addListener('click', function() {
 
-        // Changing the content of indowindow passing contentstring
-        infowindow.setContent(contentString)
+    // Click on A Marker! It shows infowindow
+    marker.addListener('click', function () {
 
-        // Opening InfoWindow
-        infowindow.open(map, marker)
-      })
+      // Changing the content of indowindow passing contentstring
+      infowindow.setContent(contentString)
+
+      // Opening InfoWindow
+      infowindow.open(map, marker)
+    })
+
+    })
+
+    
 
 
   }
@@ -83,7 +78,7 @@ class YourComponent extends Component {
 }
 
 function loadScript(url) {
-  let index  = window.document.getElementsByTagName("script")[0]
+  let index = window.document.getElementsByTagName("script")[0]
   let script = window.document.createElement("script")
   script.src = url
   script.async = true
